dcd27c86696aa804d5705a72166f690b5413b282
Luis <luissanzaguilar@gmail.com>
2025-08-11 13:45:43 +0200

    Initial commit

---
+++ src/main/java/lsz/training/web/two/webtwo/WebtwoApplication.java
@@ -0,0 +1,13 @@
+package lsz.training.web.two.webtwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lsz/training/web/two/webtwo/WebtwoApplicationTests.java
@@ -0,0 +1,13 @@
+package lsz.training.web.two.webtwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
